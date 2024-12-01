31678ea4f0bb376cad51dbe2f01991cfe6c83425
Herbds <acabohorquez43@gmail.com>
2024-12-01 01:19:29 -0500

    New_root

--- Root_locus.py
+++ Root_locus.py
@@ -18,40 +18,37 @@ st.markdown(
     </style>
     """, unsafe_allow_html=True)
 
-def main():
-    
+def root_locus():
+    m, r, d, g, l, j, Kp, Ki, Kd = define_parameters()
+    j = (2/5)*m*r*r  # Momento de inercia
 
-    
-    def root_locus():
-        m, r, d, g, l, j, Kp, Ki, Kd = define_parameters()
-        j = (2/5)*m*r*r  # Momento de inercia
+    # Fórmula para el numerador
+    numerador = (m * g * d) / (l * ((j / (r * r)) + m))
 
-        # Fórmula para el numerador
-        numerador = (m * g * d) / (l * ((j / (r * r)) + m))
-        
-        plant_tf = define_plant([numerador], [1, 0, 0])
-        pid_tf = design_pid_controller(Kp, Ki, Kd)
-        
-        #Open loop function
-        open_loop_tf = define_open_loop_system(plant_tf, pid_tf)
-        
-        
-        def plot_root_locus(sys_tf,k):
-            # Crear el gráfico del root locus con la cuadrícula activada
-            plt.figure(figsize=(9, 7))
-            cplt = ct.root_locus_plot(sys_tf, grid=True, initial_gain=k )
+    plant_tf = define_plant([numerador], [1, 0, 0])
+    pid_tf = design_pid_controller(Kp, Ki, Kd)
 
-            # Configurar el título y etiquetas
-            plt.title('Lugar de las Raíces del Sistema')
-            plt.xlabel('Parte Real')
-            plt.ylabel('Parte Imaginaria')
-            # Guardar la imagen en el directorio /content/
-            plt.show()
-        
-        plot_root_locus(open_loop_tf, pid_tf)
-    
-    root_locus()
-        
+    # Open loop function
+    open_loop_tf = define_open_loop_system(plant_tf, pid_tf)
 
-    if __name__ == "__main__":
-        main()
+    def plot_root_locus(sys_tf, k):
+        # Crear el gráfico del root locus con la cuadrícula activada
+        plt.figure(figsize=(9, 7))
+        cplt = ct.root_locus_plot(sys_tf, grid=True, initial_gain=k )
+
+        # Configurar el título y etiquetas
+        plt.title('Lugar de las Raíces del Sistema')
+        plt.xlabel('Parte Real')
+        plt.ylabel('Parte Imaginaria')
+
+        # Mostrar el gráfico en Streamlit
+        st.pyplot(plt)  # Cambié plt.show() por st.pyplot()
+
+    plot_root_locus(open_loop_tf, pid_tf)
+
+# Llamada a la función root_locus
+root_locus()
+
+if __name__ == "__main__":
+    # No es necesario agregar main() aquí si no hay otras funcionalidades principales
+    pass
