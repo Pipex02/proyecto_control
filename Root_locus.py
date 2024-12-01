3f411bc1e1fb8ef614caa3cb8277ef440581364e
Herbds <acabohorquez43@gmail.com>
2024-12-01 08:00:19 -0500

    Polos y Zeros

--- Root_locus.py
+++ Root_locus.py
@@ -2,7 +2,7 @@ import control as ct
 import numpy as np
 import matplotlib.pyplot as plt
 import streamlit as st
-from define_planta import define_parameters, define_plant, design_pid_controller, define_open_loop_system, calculate_plant_parameters
+from define_planta import define_parameters, define_plant, design_pid_controller, define_open_loop_system, calculate_plant_parameters, define_closed_loop_system
 
 # === Establecer el estilo de la página ===
 st.markdown(
@@ -12,40 +12,54 @@ st.markdown(
     .block-container {
         max-width: 72%;
         margin: auto;
-        overflow: auto;  /* Permite desplazamiento vertical si el contenido excede el área */
-        /* Reducir el margen superior para que haya menos espacio encima de la página */
-    
+        overflow: auto;
+    }
     </style>
     """, unsafe_allow_html=True)
 
-# Definir parámetros y calcular la planta
-m, r, d, g, l, j, Kp, Ki, Kd = define_parameters()
-numerador = calculate_plant_parameters(m, r, d, g, l)
-plant_num = [numerador]        # Numerador de la planta
-plant_den = [1, 0, 0]         # Denominador de la planta
+def main():
+    
+    def root_locus():
+        # Definir parámetros y calcular la planta
+        m, r, d, g, l, j, Kp, Ki, Kd = define_parameters()
+        numerador = calculate_plant_parameters(m, r, d, g, l)
+        plant_num = [numerador]        # Numerador de la planta
+        plant_den = [1, 0, 0]         # Denominador de la planta
 
-# Definir la planta
-plant_tf_sym = define_plant(plant_num, plant_den)  
-# Diseñar el controlador PID
-pid_tf = design_pid_controller(Kp, Ki, Kd)
+        # Definir la planta
+        plant_tf_sym = define_plant(plant_num, plant_den)  
+        
+        # Diseñar el controlador PID
+        pid_tf = design_pid_controller(Kp, Ki, Kd)
+        
+        # Definir sistema en lazo abierto
+        open_loop_tf = define_open_loop_system(plant_tf_sym, pid_tf)
+        
+        # Definir sistema en lazo cerrado
+        closed_loop_tf = define_closed_loop_system(open_loop_tf)
+        
+        def plot_closed_loop_poles_zeros(sys_tf):
+            # Crear el sistema en lazo cerrado con retroalimentación unitaria
+            sys_cl = ct.feedback(sys_tf, 1)
+            
+            # Crear el gráfico del lugar de las raíces utilizando ct.pole_zero_plot
+            plt.figure(figsize=(9, 7))
+            ct.pole_zero_plot(sys_cl, grid=True)
+            
+            # Configurar el título y etiquetas
+            plt.title('Lugar de las Raíces del Sistema')
+            plt.xlabel('Parte Real')
+            plt.ylabel('Parte Imaginaria')
 
-# Extraer los coeficientes numéricos del PID
-numerador_pid = pid_tf.num[0]  # Numerador de PID
-denominador_pid = pid_tf.den[0]  # Denominador de PID
+            # Mostrar el gráfico en Streamlit
+            st.pyplot(plt)
+        
+        # Mostrar la gráfica y los polos en lazo cerrado
+        plot_closed_loop_poles_zeros(open_loop_tf)
+        ct.damp(closed_loop_tf, doprint=True)
 
-# Open loop function
-open_loop_tf = define_open_loop_system(plant_tf_sym, pid_tf)
+    # Llamada a la función principal
+    root_locus()
 
-# Crear el gráfico del root locus utilizando ct.root_locus
-fig, ax = plt.subplots(figsize=(9, 7))  # Crear la figura y los ejes correctamente
-
-# Generar los puntos del root locus
-ct.root_locus(open_loop_tf, ax=ax, grid=True, initial_gain=pid_tf)
-
-# Configurar el título y etiquetas
-ax.set_title('Lugar de las Raíces del Sistema')
-ax.set_xlabel('Parte Real')
-ax.set_ylabel('Parte Imaginaria')
-
-# Mostrar el gráfico en Streamlit
-st.pyplot(fig)
+if __name__ == "__main__":
+    main()
