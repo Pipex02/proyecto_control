19fa9689ef882838304505e30edfba51e64bc732
Herbds <acabohorquez43@gmail.com>
2024-12-02 11:20:53 -0500

    Root locus maybe last update

--- Root_locus.py
+++ Root_locus.py
@@ -3,7 +3,9 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 import streamlit as st
-from Root_base import root_locus  # Asegúrate de tener la función root_locus definida en el archivo 'Root_base.py'
+from Root_base_define import root_locus  # Asegúrate de tener la función root_locus definida en el archivo 'Root_base.py'
+from Root_base_nondefine import non_root_locus  # Asegúrate de tener la función root_locus definida en el archivo 'Root_base.py'
+
 
 # === Establecer el estilo de la página ===
 st.markdown(
